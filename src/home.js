19f6743d39e03b304fa98caeb66b500ba056ce10
dardan09876 <dardan09876@gmail.com>
2025-08-26 00:32:39 -0700

    Home page

--- src/home.js
+++ src/home.js
@@ -1,4 +1,17 @@
+import restaurantImg from "./images/pexels-reneterp-1581384.jpg";
+
 export function renderHomePage(container) {
     container.innerHTML = `<h1>Home Page</h1>
-    <p>Welcome to my reataurant! Come give us a try any time, any day!</p>`;
+    <h2>My restaurant</h2>
+    <p>Welcome to my restaurant website! Come give us a try any time, any day!</p>
+    <div class="restaurant-img">
+    <img src="${restaurantImg}" alt="Cozy restaurant" />
+    </div>
+    <p>Come enjoy our cozy atmosphere and tasty food!
+    <div class="photo-credits">
+      <h2>Photo Credits</h2>
+      <ul>
+      <li>Rene Terp - <a href="https://www.pexels.com/photo/restaurant-table-and-chairs-1581384/" target="_blank">Restaurant</a></li>
+      </ul>
+    </div>`
 }
--- src/menu.js
+++ src/menu.js
@@ -14,10 +14,10 @@ export function renderMenuPage(container) {
     <div class="photo-credits">
       <h2>Photo Credits</h2>
       <ul>
-      <li>Brigitte Tohm — <a href="https://www.pexels.com/photo/french-macarons-in-bowl-239581/" target="_blank">Macarons</a></li>
-      <li>Jane T. D. — <a href="https://www.pexels.com/photo/top-view-photo-of-food-dessert-1099680/" target="_blank">Dessert</a></li>
-      <li>Ash Craig — <a href="https://www.pexels.com/photo/pancake-with-sliced-strawberry-376464/" target="_blank">Pancakes</a></li>
-      <li>Jason Villanueva — <a href="https://www.pexels.com/photo/close-up-photography-of-cup-of-coffee-851555/" target="_blank">Coffee</a></li>
+      <li>Brigitte Tohm - <a href="https://www.pexels.com/photo/french-macarons-in-bowl-239581/" target="_blank">Macarons</a></li>
+      <li>Jane T. D. - <a href="https://www.pexels.com/photo/top-view-photo-of-food-dessert-1099680/" target="_blank">Dessert</a></li>
+      <li>Ash Craig - <a href="https://www.pexels.com/photo/pancake-with-sliced-strawberry-376464/" target="_blank">Pancakes</a></li>
+      <li>Jason Villanueva - <a href="https://www.pexels.com/photo/close-up-photography-of-cup-of-coffee-851555/" target="_blank">Coffee</a></li>
       </ul>
     </div>
   `;
