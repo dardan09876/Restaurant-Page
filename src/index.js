4e9ce63def5893aa1e6ebc8c2227a287f704ce5b
dardan09876 <dardan09876@gmail.com>
2025-08-25 23:00:17 -0700

    Import styles.css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { navigateTo } from './router.js';
+import "./styles.css";
 
 document.getElementById('home-button').addEventListener('click', () => navigateTo('/'));
 document.getElementById('about-button').addEventListener('click', () => navigateTo('/about'));
